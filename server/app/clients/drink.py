6f78fae668439891c9e0355dd01d2d8d699b191d
Oved AYDIN <ovedaydin@gmail.com>
2023-01-29 22:10:24 +0300

    get random drink with make request function

--- server/app/clients/drink.py
+++ server/app/clients/drink.py
@@ -14,15 +14,7 @@ class Drink:
     def get_categories(self):
         return self.make_request(f'{self.url}/list.php?c=list')
 
-    def get_random_drink(self):
-        #     content = request.get_json()
-        # category = content['selected']
-        # url = (
-        #     f'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={category}'
-        # )
-        # response = requests.get(url)
-        # data = response.json()
-        # all_drinks = len(data['drinks'])
-        # random_num = random.randint(0, int(all_drinks))
-        # drink = data['drinks'][random_num]
-        pass
+    def get_random_drink(self, category):
+        data = self.make_request(f'{self.url}/filter.php?c={category}')
+        random_num = random.randint(0, len(data['drinks']))
+        return data['drinks'][random_num]
