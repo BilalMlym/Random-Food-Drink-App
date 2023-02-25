00c151075094d6482d4eb1b011036f7e62bae103
Oved AYDIN <ovedaydin@gmail.com>
2023-02-25 22:43:05 +0300

    add loader to random meal

--- src/components/Meal/Meal.jsx
+++ src/components/Meal/Meal.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import MealCategory from './MealCategory/MealCategory';
 import RandomMeal from './RandomMeal/RandomMeal';
 
 const Meal = () => {
   const [mealCategory, setMealCategory] = useState(null);
-  useEffect(() => {
-    console.log(mealCategory);
-  }, [mealCategory]);
+
   return (
     <div>
       <MealCategory category={mealCategory} setCategory={setMealCategory} />
